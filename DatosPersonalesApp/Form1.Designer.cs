045ecd40a4d8547c3829d5eab25fa5669eca5d26
FrancoGentil <francogentiltolra@gmail.com>
2025-08-12 20:26:46 -0300

    Primera subida...

---
+++ DatosPersonalesApp/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace DatosPersonalesApp
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.Apellido = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.Edad = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.Direccion = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(196, 65);
+            this.txtApellido.MaxLength = 30;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(390, 26);
+            this.txtApellido.TabIndex = 0;
+            // 
+            // Apellido
+            // 
+            this.Apellido.AutoSize = true;
+            this.Apellido.Location = new System.Drawing.Point(83, 71);
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Size = new System.Drawing.Size(88, 20);
+            this.Apellido.TabIndex = 1;
+            this.Apellido.Text = "APELLIDO";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(83, 124);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(79, 20);
+            this.Nombre.TabIndex = 2;
+            this.Nombre.Text = "NOMBRE";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(196, 118);
+            this.txtNombre.MaxLength = 30;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(390, 26);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // Edad
+            // 
+            this.Edad.AutoSize = true;
+            this.Edad.Location = new System.Drawing.Point(83, 174);
+            this.Edad.Name = "Edad";
+            this.Edad.Size = new System.Drawing.Size(55, 20);
+            this.Edad.TabIndex = 4;
+            this.Edad.Text = "EDAD";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(196, 168);
+            this.txtEdad.MaxLength = 3;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(85, 26);
+            this.txtEdad.TabIndex = 5;
+            // 
+            // Direccion
+            // 
+            this.Direccion.AutoSize = true;
+            this.Direccion.Location = new System.Drawing.Point(83, 220);
+            this.Direccion.Name = "Direccion";
+            this.Direccion.Size = new System.Drawing.Size(99, 20);
+            this.Direccion.TabIndex = 6;
+            this.Direccion.Text = "DIRECCION";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(196, 214);
+            this.txtDireccion.MaxLength = 50;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(390, 26);
+            this.txtDireccion.TabIndex = 7;
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(83, 304);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(108, 20);
+            this.Resultado.TabIndex = 8;
+            this.Resultado.Text = "RESULTADO";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(87, 327);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(499, 221);
+            this.txtResultado.TabIndex = 10;
+            this.txtResultado.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 595);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 38);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(357, 595);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 38);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(678, 694);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.Direccion);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.Edad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.Apellido);
+            this.Controls.Add(this.txtApellido);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Formulario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label Apellido;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label Edad;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label Direccion;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.RichTextBox txtResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DatosPersonalesApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatosPersonalesApp
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        //Evento del boton aceptar para validar los campos y mostrar los datos ingresados
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Convertir todo a mayúsculas
+            txtApellido.Text = txtApellido.Text.ToUpper();
+            txtNombre.Text = txtNombre.Text.ToUpper();
+            txtEdad.Text = txtEdad.Text.ToUpper();
+            txtDireccion.Text = txtDireccion.Text.ToUpper();
+
+            // Limitar caracteres (esto también deberías establecer en el diseñador si querés)
+            txtApellido.MaxLength = 30;
+            txtNombre.MaxLength = 30;
+            txtEdad.MaxLength = 3;
+            txtDireccion.MaxLength = 50;
+
+            // Limpiar colores previos
+            txtApellido.BackColor = Color.White;
+            txtNombre.BackColor = Color.White;
+            txtEdad.BackColor = Color.White;
+            txtDireccion.BackColor = Color.White;
+
+            bool hayError = false;
+
+            // Validar campos vacíos
+            if (string.IsNullOrWhiteSpace(txtApellido.Text))
+            {
+                txtApellido.BackColor = Color.Red;
+                hayError = true;
+            }
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                txtNombre.BackColor = Color.Red;
+                hayError = true;
+            }
+            if (string.IsNullOrWhiteSpace(txtEdad.Text))
+            {
+                txtEdad.BackColor = Color.Red;
+                hayError = true;
+            }
+            if (string.IsNullOrWhiteSpace(txtDireccion.Text))
+            {
+                txtDireccion.BackColor = Color.Red;
+                hayError = true;
+            }
+
+            // Validar que Edad tenga solo números
+            if (!int.TryParse(txtEdad.Text, out int edad))
+            {
+                txtEdad.BackColor = Color.Red;
+                hayError = true;
+            }
+
+            // Si hay error, no continuar
+            if (hayError)
+            {
+                MessageBox.Show("Por favor complete correctamente todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Mostrar datos en el TextBox multilinea
+            txtResultado.Text = $"Apellido y Nombre: {txtApellido.Text} {txtNombre.Text}\r\n" +
+                                $"Edad: {txtEdad.Text}\r\n" +
+                                $"Dirección: {txtDireccion.Text}";
+        }
+
+        //Evento del boton cancelar para salir de la aplicación
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Evento para validar que solo se ingresen números en el campo Edad
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea la tecla
+            }
+        }
+    }
+}
